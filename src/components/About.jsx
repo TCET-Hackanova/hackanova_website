0b1c769afc031e460950d3a2b754b43db9dd49e0
dhyey <d1373@protonmail.com>
2025-01-24 03:05:16 +0530

    dumb things

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,80 +1,101 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-  import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import AnimatedTitle, { SAnimatedTitle } from "./AnimatedTitle";
+import React, { useEffect } from 'react';
 
 // import CardGallery from "./Bento";
-import {  BentoGridThirdDemo } from "./Facts";
+import { BentoGridThirdDemo } from "./Facts";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
-
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  useGSAP(() => {
-    const clipAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#clip",
-        start: "center center",
-        end: "+=800 center",
-        scrub: 0.5,
-        pin: true,
-        pinSpacing: true,
-      },
+  useEffect(() => {
+      // Create a script element
+      const script = document.createElement("script");
+      script.src = "https://apply.devfolio.co/v2/sdk.js";
+      script.async = true;
+      script.defer = true;
+
+      // Append the script to the document body
+      document.body.appendChild(script);
+
+      // Cleanup: Remove the script when the component is unmounted
+      return () => {
+          document.body.removeChild(script);
+      };
+  }, []);
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            },
+        });
+
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0,
+        });
+
+        // Rotate the rings
+        gsap.to(".planet-rings", {
+            rotation: 360,
+            duration: 20,
+            repeat: -1,
+            ease: "linear",
+        });
     });
 
-    clipAnimation.to(".mask-clip-path", {
-      width: "100vw",
-      height: "100vh",
-      borderRadius: 0,
-    });
+    return (
 
-    // Rotate the rings
-    gsap.to(".planet-rings", {
-      rotation: 360,
-      duration: 20,
-      repeat: -1,
-      ease: "linear",
-    });
-  });
+        <div id='about' className=' w-screen'>
+            <div className='z- mb-24 mt-52 flex flex-col items-center gap-2 '>
+                {/* <div className="z-10 before:bg-[radial-gradient(50%_100%,#8E24AA,transparent)] relative before:absolute before:inset-[-10%_-2%] before:-z-10 before:blur-3xl before:rounded-t-full"> */}
 
-  return (
-      <div id='about' className=' w-screen'>
-          <div className='z- mb-24 mt-52 flex flex-col items-center gap-2 '>
-              {/* <div className="z-10 before:bg-[radial-gradient(50%_100%,#8E24AA,transparent)] relative before:absolute before:inset-[-10%_-2%] before:-z-10 before:blur-3xl before:rounded-t-full"> */}
+                <SAnimatedTitle
+                    title='TSDW Presents'
+                    containerClass='mt-5 !text-white  text-center'
+                />
+                <AnimatedTitle
+                    title='Hack<b>a</b>Nova 4.0'
+                    containerClass=' bg-gradient-to-bl from-blue-500 to-blue-800 bg-clip-text text-transparent leading-normal   text-center'
+                />
 
-              <SAnimatedTitle
-                  title='TSDW Presents'
-                  containerClass='mt-5 !text-white  text-center'
-              />
-              <AnimatedTitle
-                  title='Hack<b>a</b>Nova 4.0'
-                  containerClass=' bg-gradient-to-bl from-blue-500 to-blue-800 bg-clip-text text-transparent leading-normal   text-center'
-              />
+                {/* </div> */}
 
-              {/* </div> */}
+                {/* <a
+                    href='https://devfolio.co'
+                    className='bg-white rounded-lg p-3 flex items-center shadow-md hover:shadow-lg transition mb-20'
+                >
+                    <div className='flex items-center'>
+                        <img
+                            src='./img/devfolio.png'
+                            className='h-6 mr-2'
+                            alt='DEVFOLIO LOGO'
+                        />
+                        <span>
+                            <b>Apply with Devfolio</b>
+                        </span>
+                    </div>
+                </a> */}
+                <div
+                    data-hackathon-slug='hackanova-5'
+                    data-button-theme='dark-inverted'
+                    style='height: 44px; width: 312px'
+                ></div>
 
-              <a
-                  href='https://devfolio.co'
-                  className='bg-white rounded-lg p-3 flex items-center shadow-md hover:shadow-lg transition mb-20'
-              >
-                  <div className='flex items-center'>
-                      <img
-                          src='./img/devfolio.png'
-                          className='h-6 mr-2'
-                          alt='DEVFOLIO LOGO'
-                      />
-                      <span>
-                          <b>Apply with Devfolio</b>
-                      </span>
-                  </div>
-              </a>
-
-              <BentoGridThirdDemo />
-          </div>
-      </div>
-  );
+                <BentoGridThirdDemo />
+            </div>
+        </div>
+    );
 };
 
 export default About;
