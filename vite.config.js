582d1f298ca2fb0f70c49e27798d867b6209a448
TCET-Hackanova <116735528+TCET-Hackanova@users.noreply.github.com>
2025-01-23 22:49:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HackAnova4.0/'  , // Replace <repository-name> with your GitHub repository name
+  base: '/hackanova_website/'  , // Replace <repository-name> with your GitHub repository name
 });
