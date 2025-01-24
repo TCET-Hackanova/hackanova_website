c1a1fc93fc3768aad371cca9a9d9c89108cd613a
Neel jain <nj6097114@gmail.com>
2025-01-24 23:48:07 +0530

    discord button

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -1,6 +1,6 @@
 // components/FloatingButton.js
 import React from "react";
-import { BsTelegram } from "react-icons/bs";
+import { BsDiscord, BsTelegram } from "react-icons/bs";
 
 const FloatingButton = () => {
  
@@ -8,10 +8,10 @@ const FloatingButton = () => {
   return (
     <button
       
-      className="fixed bottom-5 right-5 bg-purple-600 hover:bg-purple-700 text-white w-14 h-14 rounded-full shadow-2xl shadow-gray-500 flex justify-center items-center transition-transform transform hover:scale-110"
+      className="z-50 fixed bottom-5 right-5 bg-purple-600 hover:bg-purple-700 text-white w-14 h-14 rounded-full shadow-2xl shadow-gray-500 flex justify-center items-center transition-transform transform hover:scale-110"
     >
-    <a target="_blank" href="https://t.me/+T-xYOtvS4IwwY2U9">
-    <BsTelegram className="text-2xl" />
+    <a target="_blank" href="https://discord.gg/EejJwhAG">
+    <BsDiscord className="text-2xl z-50" />
     </a>
     </button>
   );
