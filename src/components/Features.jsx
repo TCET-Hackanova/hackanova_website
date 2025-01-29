53b82f3de3bb150ce0cfe19e728172923d9bf80d
dhyey <d1373@protonmail.com>
2025-01-29 15:52:45 +0530

    added aptos logo

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -80,51 +80,56 @@ export const BentoCard = ({ src,dlink, title, description, isComingSoon }) => {
 };
 
 const Features = () => (
-  <section className="mt-5">
-    <div className="container mx-auto px-3 md:px-10">
-      <div className="px-5 py-32">
-        <p className="font-circular-web text-center text-3xl lg:text-4xl font-bold   text-blue-50">
-         Meet Our <span className="bg-purple-800"> Sponsors</span>
-        
-        </p>
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-        
-        </p>
-      </div>
+    <section className='mt-5'>
+        <div className='container mx-auto px-3 md:px-10'>
+            <div className='px-5 py-32'>
+                <p className='font-circular-web text-center text-3xl lg:text-4xl font-bold   text-blue-50'>
+                    Meet Our <span className='bg-purple-800'> Sponsors</span>
+                </p>
+                <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'></p>
+            </div>
 
-   
-      <div className="grid gap-4 w-full  md:gap-7 lg:grid-cols-4">
-  <BentoTilt className="bento-tilt_1 md:col-span-1">
-    <BentoCard
-      src="videos/feature-2.mp4"
-      title={
-        <img src="./img/Devfolio_Logo-White.png" className="h-12" alt="DEVFOLIO LOGO"/>
-      }
-      dlink="https://devfolio.co"
-      
-    />
-  </BentoTilt>
-  <BentoTilt className="bento-tilt_1 md:col-span-1">
-    <BentoCard
-      src="videos/feature-2.mp4"
-      title={
-        <img src="./img/Polygon_Logo-White@2x.png" className="h-12" alt="POLYGON LOGO"/>
-      }
-      dlink="https://polygon.technology/"
-      
-    />
-  </BentoTilt>
-  <BentoTilt className="bento-tilt_1 md:col-span-1">
-    <BentoCard
-      src="videos/feature-2.mp4"
-      title={
-        <img src="./img/ethindia-light.png" alt="ETHINDIA LOGO" className="h-12"/>
-      }
-      dlink="https://ethindia.co"
-      
-    />
-  </BentoTilt>
-  {/* <BentoTilt className="bento-tilt_1 md:col-span-1">
+            <div className='grid gap-4 w-full  md:gap-7 lg:grid-cols-4'>
+                <BentoTilt className='bento-tilt_1 md:col-span-1'>
+                    <BentoCard
+                        src='videos/feature-2.mp4'
+                        title={
+                            <img
+                                src='./img/Devfolio_Logo-White.png'
+                                className='h-12'
+                                alt='DEVFOLIO LOGO'
+                            />
+                        }
+                        dlink='https://devfolio.co'
+                    />
+                </BentoTilt>
+                <BentoTilt className='bento-tilt_1 md:col-span-1'>
+                    <BentoCard
+                        src='videos/feature-2.mp4'
+                        title={
+                            <img
+                                src='./img/Polygon_Logo-White@2x.png'
+                                className='h-12'
+                                alt='POLYGON LOGO'
+                            />
+                        }
+                        dlink='https://polygon.technology/'
+                    />
+                </BentoTilt>
+                <BentoTilt className='bento-tilt_1 md:col-span-1'>
+                    <BentoCard
+                        src='videos/feature-2.mp4'
+                        title={
+                            <img
+                                src='./img/ethindia-light.png'
+                                alt='ETHINDIA LOGO'
+                                className='h-12'
+                            />
+                        }
+                        dlink='https://ethindia.co'
+                    />
+                </BentoTilt>
+                {/* <BentoTilt className="bento-tilt_1 md:col-span-1">
     <BentoCard
       src="videos/feature-2.mp4"
       title={
@@ -134,45 +139,61 @@ const Features = () => (
       
     />
   </BentoTilt> */}
-  <BentoTilt className="bento-tilt_1 md:col-span-1">
-    <BentoCard
-      src="videos/feature-2.mp4"
-      title={
-        <img src="./img/xyz-logo-white.png" className="h-12" alt=".XYZ LOGO"/>
-      }
-      dlink="https://gen.xyz/"
-      
-    />
-  </BentoTilt>
-</div>
-
-    </div>
-    <Slider/>
-    <div className=" flex lg:mx-52 mt-24 mx-8">
-    <div className="container z-10 relative before:absolute before:bg-gradient-to-b from-purple-600 via-purple-800 to-black  before:-inset-2 before:-z-10 before:blur-3xl before:round-[inherit]">
-		
-    <FaqTeachers faqs={faqs}/>
-    
-		</div>
-    
-    </div>
-    <div className="mt-10 p-10 dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2 justify-center items-center relative overflow-hidden">
-    <div className="p-2  lg:w-2/5 mx-auto ">
-    <h1 className="text-5xl text-white font-bold text-center">Meet the Team</h1>
-    <BentoGridItem 
-    title="TSDW Technical Team"
-    description="The TSDW Technical Team is the backbone of Hackanova, ensuring seamless execution with cutting-edge technologies and innovative solutions.
-From setting up platforms to troubleshooting in real-time, the team fosters a collaborative environment for an exceptional hackathon experience."
-     className={"mt-5"}
-    header=
-    {
-      <img src="./images/a.jpeg" alt="" className="h-full object-cover object-center aspect-auto-16/9" />
-    }
-    />
-    </div>
-    </div>
-   
-  </section>
+                <BentoTilt className='bento-tilt_1 md:col-span-1'>
+                    <BentoCard
+                        src='videos/feature-2.mp4'
+                        title={
+                            <img
+                                src='./img/xyz-logo-white.png'
+                                className='h-12'
+                                alt='.XYZ LOGO'
+                            />
+                        }
+                        dlink='https://gen.xyz/'
+                    />
+                </BentoTilt>
+                <BentoTilt className='bento-tilt_1 md:col-span-1'>
+                    <BentoCard
+                        src='videos/feature-2.mp4'
+                        title={
+                            <img
+                                src='./img/Aptos_Primary_WHT.png'
+                                className='h-12'
+                                alt='.XYZ LOGO'
+                            />
+                        }
+                        dlink='https://aptosfoundation.org/'
+                    />
+                </BentoTilt>
+            </div>
+        </div>
+        <Slider />
+        <div className=' flex lg:mx-52 mt-24 mx-8'>
+            <div className='container z-10 relative before:absolute before:bg-gradient-to-b from-purple-600 via-purple-800 to-black  before:-inset-2 before:-z-10 before:blur-3xl before:round-[inherit]'>
+                <FaqTeachers faqs={faqs} />
+            </div>
+        </div>
+        <div className='mt-10 p-10 dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2 justify-center items-center relative overflow-hidden'>
+            <div className='p-2  lg:w-2/5 mx-auto '>
+                <h1 className='text-5xl text-white font-bold text-center'>
+                    Meet the Team
+                </h1>
+                <BentoGridItem
+                    title='TSDW Technical Team'
+                    description='The TSDW Technical Team is the backbone of Hackanova, ensuring seamless execution with cutting-edge technologies and innovative solutions.
+From setting up platforms to troubleshooting in real-time, the team fosters a collaborative environment for an exceptional hackathon experience.'
+                    className={"mt-5"}
+                    header={
+                        <img
+                            src='./images/a.jpeg'
+                            alt=''
+                            className='h-full object-cover object-center aspect-auto-16/9'
+                        />
+                    }
+                />
+            </div>
+        </div>
+    </section>
 );
 
 export default Features;
