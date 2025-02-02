24b008ab23b913c999731cb72abb0b910d6fe29c
dhyey <d1373@protonmail.com>
2025-02-02 13:41:02 +0530

    added Hardware Track

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -111,10 +111,10 @@ export function TimelineDemo() {
       <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
       <Timeline data={data} />
             </p>
-      {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-      <Timeline2 data={Hardware_data} /> */}
-      {/* </p> */}
+      <Timeline2 data={Hardware_data} />
+      </p>
     </div>
   );
 }
